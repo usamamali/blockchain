070f585fc2be5f3c5964023b04c6285b4c0982ab
Usama M. Ali <eng.usama.muhammad@gmail.com>
2018-04-01 08:04:38 +0400

    testing with mocha .. sample

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,3 +2,28 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+
+class Car {
+    park(){
+        return 'stopped';
+    }
+
+    drive(){
+        return 'vroom';
+    }
+}
+let car;
+
+beforeEach(() => {
+    car = new Car();
+});
+
+describe ('Car Class',() => {
+    it('can park',() => {
+        assert.equal(car.park(), 'stopped');
+    });
+
+    it('can drive',() => {
+        assert.equal(car.drive(), 'vroom');
+    });
+});
