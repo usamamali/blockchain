f73c4a87199d229810ca49216f1566b2e934d3d6
Usama M. Ali <eng.usama.muhammad@gmail.com>
2018-04-07 17:05:08 +0400

    add lottery dependencies and contract

---
+++ lottery/contracts/Lottery.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.4.17;
+
+contract Lottery {
+    address public manager;
+    address[] private players;
+
+    function Lottery() public {
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > .01 ether);
+        players.push(msg.sender);
+    }
+
+    function pickWinner() public restricted {
+        uint winningPlayerIndex = getRandomNumber() % players.length;
+        address winningPlayer = players[winningPlayerIndex];
+        winningPlayer.transfer(this.balance);
+        
+        // reset lottery game
+        players = new address[](3);
+    }
+
+    modifier restricted () {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function getRandomNumber() private view returns(uint) {
+        return uint(keccak256(block.difficulty, now, players));
+    }
+
+    function getPlayers() public view restricted returns (address[]){
+        return players;
+    }
+}
